e1e0e95d3560f076d9ac8e0341899626b5d3c842
Linh M. Tran <linh.mtran168@live.com>
2012-05-03 17:08:17 +0700

    Complete static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                               :text => "#{base_title} | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+                               :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                               :text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+
+end
