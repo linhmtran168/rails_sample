d4af8c218586d1f68607ded1153647cad29ae0c9
Linh M. Tran <linh.mtran168@live.com>
2012-05-07 15:12:41 +0700

    Finish checking

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "error message" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -36,11 +43,19 @@ describe "User pages" do
         fill_in "Name", with: "Abcxyz"
         fill_in "Email", with: "abc@abc.com"
         fill_in "Password", with: "abcxyz"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirmation", with: "abcxyz"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email("abc\@abc.com") }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
