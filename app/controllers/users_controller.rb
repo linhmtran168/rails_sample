74547282ba70077588a4d5697fe2c52c7f897cd8
Linh M. Tran <linh.mtran168@live.com>
2012-05-08 16:58:35 +0700

    Finish updatint user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+  before_filter :non_signed_in, only: [:new, :create]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -19,4 +28,54 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.admin?
+      flash[:notice] = "You cannot delete admin user"
+      redirect_to root_path
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_path
+    end
+  end
+
+  private 
+  
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notices: "Please sign in." 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def non_signed_in
+      redirect_to(root_path) unless !signed_in?
+    end
 end
